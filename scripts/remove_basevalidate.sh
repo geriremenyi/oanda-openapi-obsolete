dd445af274530424d61ff4c3f5fad34f95345645
Gergely Reményi <geri@geriremenyi.com>
2020-08-25 18:33:00 +0200

    Fixing some .NET core codegen issues

---
+++ scripts/remove_basevalidate.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# This is needed because of the bug 
+# https://github.com/OpenAPITools/openapi-generator/issues/5416 in the openapi-generator
+
+# Removing 'foreach(var x in BaseValidate(validationContext)) yield return x;' lines
+for model_file in ./out/code/csharp-netcore/src/GeriRemenyi.OANDA.V20/Model/*.cs; do
+    sed -i "s/foreach(var x in BaseValidate(validationContext)) yield return x;//g" "$model_file"
+done
