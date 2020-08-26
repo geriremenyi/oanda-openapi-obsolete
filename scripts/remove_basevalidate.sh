2678286b6e6d298654bf11f240c501c299bbfadf
Gergely Reményi <geri@geriremenyi.com>
2020-08-26 15:55:28 +0200

    Workflow debug

--- scripts/remove_basevalidate.sh
+++ scripts/remove_basevalidate.sh
@@ -4,6 +4,7 @@
 # https://github.com/OpenAPITools/openapi-generator/issues/5416 in the openapi-generator
 
 # Removing 'foreach(var x in BaseValidate(validationContext)) yield return x;' lines
+shopt -s globstar
 for model_file in ./out/code/csharp-netcore/src/GeriRemenyi.OANDA.V20/Model/*.cs; do
     echo "$model_file"
     # sed -i "s/foreach(var x in BaseValidate(validationContext)) yield return x;//g" "$model_file"
