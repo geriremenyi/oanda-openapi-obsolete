9bf17efdcdaef15126a199c1728b96a5a2d407b5
Gergely Reményi <geri@geriremenyi.com>
2020-08-27 21:51:14 +0200

    Removing inline response types first step

--- scripts/remove_basevalidate.sh
+++ scripts/remove_basevalidate.sh
@@ -4,6 +4,6 @@
 # https://github.com/OpenAPITools/openapi-generator/issues/5416 in the openapi-generator
 
 # Removing 'foreach(var x in BaseValidate(validationContext)) yield return x;' lines
-for model_file in ./out/code/csharp-netcore/src/GeriRemenyi.Oanda.V20/Model/*.cs; do
+for model_file in ./out/code/csharp-netcore/src/*/Model/*.cs; do
     sed -i "s/foreach(var x in BaseValidate(validationContext)) yield return x;//g" "$model_file"
 done
