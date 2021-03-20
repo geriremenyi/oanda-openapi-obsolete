e53536ca5376e984417f9682fb7a8785c1b96aa0
Gergely Reményi <geri@geriremenyi.com>
2021-03-21 00:14:38 +0100

    More script improvements

--- scripts/get_package_variable.sh
+++ scripts/get_package_variable.sh
@@ -2,8 +2,8 @@
 
 # Parameter to named parameter
 name=$([ ! -z $1 ] && echo "$1" || [ ! -z "$GET_PACKAGE_VARIABLE_NAME" ] && echo "$GET_PACKAGE_VARIABLE_NAME" || echo "")
+# Check that all parameters are given
 if [ -z $name ]; then
-    # Param is not set at all
     echo "::error file=get_package_variable.sh,line=7::\
     The variable name to get is not given. \
     Either pass the variable name as the first parameter to the script or set the GET_PACKAGE_VARIABLE_NAME environment variable."
@@ -11,7 +11,6 @@ if [ -z $name ]; then
 fi
 
 # Parse the package variable out of npm environment variables
-echo "::group::Getting '$name' from package.json"
 npm_variable_name="npm_package_$name"
 echo "The npm environment variable name is '$npm_variable_name'."
 value=$(npm run env | grep "$npm_variable_name" | cut -d '=' -f 2)
@@ -19,4 +18,3 @@ if [ -z "$value" ]
     echo "::warning file=get_package_variable.sh,line=19::The gathered package variable is empty."
 fi
 echo "::set-output name=$name::$value"
-echo "::endgroup::"
--- scripts/git_push.sh
+++ scripts/git_push.sh
@@ -1,61 +1,57 @@
 #!/bin/sh
 
 # Parameter to named parameter
-git_commit_message=$1
-git_author_email=$2
-git_author_name=$3
-
+git_commit_message=`[ ! -z $1 ] && echo "$1" || [ ! -z "$GIT_PUSH_COMMIT_MESSAGE" ] && echo "$GIT_PUSH_COMMIT_MESSAGE" || echo ""`
+git_author_email=`[ ! -z $2 ] && echo "$2" || [ ! -z "$GIT_PUSH_AUTHOR_EMAIL" ] && echo "$GIT_PUSH_AUTHOR_EMAIL" || echo ""`
+git_author_name=`[ ! -z $3 ] && echo "$3" || [ ! -z "$GIT_PUSH_AUTHOR_NAME" ] && echo "$GIT_PUSH_AUTHOR_NAME" || echo ""`
 # Check that all parameters are given
-if [ "$git_commit_message" = "" ]; then
-    if [ -z "$GIT_PUSH_COMMIT_MESSAGE" ]; then
-        echo "[ERROR] No commit message given. Please pass a commit message to the script as the first parameter."
-        exit 1
-    else
-        git_commit_message="$GIT_PUSH_COMMIT_MESSAGE"
-    fi
+if [ -z "$git_commit_message" ]; then
+    echo "::error file=git_push.sh,line=9::\
+    The commit message is not given. \
+    Either pass the commit message as the first parameter to the script or set the GIT_PUSH_COMMIT_MESSAGE environment variable."
+    exit 1
 fi
-
-if [ "$git_author_email" = "" ]; then
-    if [ -z "$GIT_PUSH_AUTHOR_EMAIL" ]; then
-        echo "[ERROR] No author email given. Please pass an author email address to the script as the second parameter."
-        exit 1
-    else
-        git_author_email="$GIT_PUSH_AUTHOR_EMAIL"
-    fi
+if [ -z "$git_author_email" ]; then
+    echo "::error file=git_push.sh,line=15::\
+    The author email is not given. \
+    Either pass the author email as the second parameter to the script or set the GIT_PUSH_AUTHOR_EMAIL environment variable."
+    exit 1
 fi
-
-if [ "$git_author_name" = "" ]; then
-    if [ -z "$GIT_PUSH_AUTHOR_NAME" ]; then
-        echo "[ERROR] No author name given. Please pass an author name to the script as the third parameter."
-        exit 1
-    else
-        git_author_name="$GIT_PUSH_AUTHOR_NAME"
-    fi
+if [ -z "$git_author_name" ]; then
+    echo "::error file=git_push.sh,line=21::\
+    The author name is not given. \
+    Either pass the author name as the third parameter to the script or set the GIT_PUSH_AUTHOR_NAME environment variable."
+    exit 1
 fi
 
 # Check that the current working directory is a root directory of a git repo
 if ! [ -d .git ]; then
-    echo "[ERROR] Current working directory is not a root directory of a git repository."
+    echo "::error file=git_push.sh,line=29::Current working directory is not a root directory of a git repository."
     exit 1
 fi
 
 # Check that the current working directory has a remote set
 git_remote=`git remote`
-if [ "$git_remote" = "" ]; then
-    echo "[ERROR] No remote repository found for the git repository."
+if [ -z "$git_remote" ]; then
+    echo "::error file=git_push.sh,line=36::No remote repository found for the git repository."
     exit 1
 fi
 
 # Set author
+echo "Setting the git author email to '***'."
 git config --global user.email "$git_author_email"
+echo "Setting the git author name to '***'."
 git config --global user.name "$git_author_name"
 
 # Add all changes
+echo "Adding all changes from the root."
 git add .
 
 # Commit all changes
+echo "Commiting the change with the commit message: '$git_commit_message'."
 git commit -m "$git_commit_message"
 
 # Push all changes
 git_branch=$(git rev-parse --abbrev-ref HEAD)
+echo "Pushing all changes to the '$git_branch' branch."
 git push origin "$git_branch"
