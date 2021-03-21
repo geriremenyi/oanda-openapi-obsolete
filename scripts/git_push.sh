60d79816d2021775c30af621dfb1072778285bc2
Gergely Reményi <geri@geriremenyi.com>
2021-03-21 12:36:58 +0100

    Test

--- scripts/git_push.sh
+++ scripts/git_push.sh
@@ -3,29 +3,23 @@
 # Parameter to named parameter
 git_commit_message=`[ ! -z $1 ] && echo "$1" || [ ! -z "$GIT_PUSH_COMMIT_MESSAGE" ] && echo "$GIT_PUSH_COMMIT_MESSAGE" || echo ""`
 git_author_email=`[ ! -z $2 ] && echo "$2" || [ ! -z "$GIT_PUSH_AUTHOR_EMAIL" ] && echo "$GIT_PUSH_AUTHOR_EMAIL" || echo ""`
-git_author_name=`[ ! -z $3 ] && echo "$3" || [ ! -z "$GIT_PUSH_AUTHOR_NAME" ] && echo "$GIT_PUSH_AUTHOR_NAME" || echo ""`
+git_author_name=`[ ! -z $2 ] && echo "$2" || [ ! -z "$GIT_PUSH_AUTHOR_NAME" ] && echo "$GIT_PUSH_AUTHOR_NAME" || echo ""`
 # Check that all parameters are given
 if [ -z "$git_commit_message" ]; then
-    echo "::error file=git_push.sh::\
-    The commit message is not given. \
-    Either pass the commit message as the first parameter to the script or set the GIT_PUSH_COMMIT_MESSAGE environment variable."
+    echo "::error file=git_push.sh::The commit message is not given. Either pass the commit message as the first parameter to the script or set the GIT_PUSH_COMMIT_MESSAGE environment variable."
     exit 1
 fi
 if [ -z "$git_author_email" ]; then
-    echo "::error file=git_push.sh::\
-    The author email is not given. \
-    Either pass the author email as the second parameter to the script or set the GIT_PUSH_AUTHOR_EMAIL environment variable."
+    echo "::error file=git_push.sh::The author email is not given. Either pass the author email as the second parameter to the script or set the GIT_PUSH_AUTHOR_EMAIL environment variable."
     exit 1
 fi
 if [ -z "$git_author_name" ]; then
-    echo "::error file=git_push.sh::\
-    The author name is not given. \
-    Either pass the author name as the third parameter to the script or set the GIT_PUSH_AUTHOR_NAME environment variable."
+    echo "::error file=git_push.sh::The author name is not given. Either pass the author name as the third parameter to the script or set the GIT_PUSH_AUTHOR_NAME environment variable."
     exit 1
 fi
 
 # Check that the current working directory is a root directory of a git repo
-if ! [ -d .git ]; then
+if [ ! -d .git ]; then
     echo "::error file=git_push.sh::Current working directory is not a root directory of a git repository."
     exit 1
 fi
@@ -39,9 +33,9 @@ fi
 
 # Set author
 echo "Setting the git author email to '***'."
-git config --global user.email "$git_author_email"
+#git config --global user.email "$git_author_email"
 echo "Setting the git author name to '***'."
-git config --global user.name "$git_author_name"
+#git config --global user.name "$git_author_name"
 
 # Add all changes
 echo "Adding all changes from the root."
